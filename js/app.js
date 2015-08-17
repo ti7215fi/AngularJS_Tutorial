83cd57766e3cb193625a80cf182306c7fcf9f295
Tim Fischer <tim@debian-entwicklung>
2015-08-17 15:43:14 +0200

    17.08.15 - first commit

---
+++ js/app.js
@@ -0,0 +1,79 @@
+'use strict';
+
+angular
+        .module('tutorialApp', ['ngAnimate', 'ngRoute'])               
+        .factory('Cart', CartFactory)
+        .controller('ArticlesCtrl', ArticlesController)
+        .controller('CartCtrl', CartController)
+        .directive('price', PriceFunction)
+        .config(RouteFunction)
+
+function CartFactory(){
+    var items = [];
+    var actions = {
+      getItems: getItems,
+      addArticle: addArticle,
+      sum: sum
+    };
+    return actions;
+    
+    function getItems()
+    {
+        return items;
+    };
+    
+    function addArticle(article){
+        items.push(article);
+    };
+    
+    function sum()
+    {
+        return items.reduce(function(total, article) {
+                return total + article.price;
+            }, 0);
+    }
+};
+
+function ArticlesController($scope, $http, Cart)
+{
+   $http.get('data/articles.json').then(function(articleResponse) {
+       $scope.cart = Cart;
+       $scope.articles = articleResponse.data; 
+   });
+};
+
+function CartController($scope, Cart)
+{
+    $scope.cart = Cart;
+};
+
+function PriceFunction()
+{
+   var restrict = 'E';
+    var scope = {
+        value: '='
+    };
+    var template = 'templates/price.tpl.html';
+    var directive = {
+      restrict      : restrict,
+      scope         : scope,
+      templateUrl   : template
+    };
+    
+    return directive;
+};
+
+function RouteFunction($routeProvider)
+{
+    var templateUrls = ['articles.html', 'about.html', 'impressum.html'];
+    
+    $routeProvider
+            .when('/',          { templateUrl: templateUrls[0]})
+            .when('/about',     { templateUrl: templateUrls[1]})
+            .when('/impressum', { templateUrl: templateUrls[2]})
+            .otherwise({ redirectTo: '/' });
+};
+
+
+           
+        
