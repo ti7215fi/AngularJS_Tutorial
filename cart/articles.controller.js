fcc7bd213839d0130744e3a1ddfc617bec52f16f
Tim Fischer <tim.fischer@fh-erfurt.de>
2015-08-18 15:26:06 +0200

    18.08.2015 -> Struktur geändert

--- cart/articles.controller.js
+++ cart/articles.controller.js
@@ -2,7 +2,7 @@ app.controller('ArticlesCtrl', ArticlesController)
 
 function ArticlesController($scope, $http, Cart)
 {   
-   $http.get('data/articles.json').then(function(articleResponse) {
+   $http.get('articles.json').then(function(articleResponse) {
        $scope.cart = Cart;
        $scope.articles = articleResponse.data; 
    });
--- cart/price.directive.js
+++ cart/price.directive.js
@@ -6,7 +6,7 @@ function PriceFunction()
     var scope = {
         value: '='
     };
-    var template = 'templates/price.tpl.html';
+    var template = 'price.tpl.html';
     var directive = {
       restrict      : restrict,
       scope         : scope,
